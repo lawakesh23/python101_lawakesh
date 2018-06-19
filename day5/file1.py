7168d85e0df76f99792f2c43cb0d844c21a8aaef
lawakesh23 <lawakesh23@gmail.com>
2018-06-19 12:07:54 -0700

    day5

---
+++ day5/file1.py
@@ -0,0 +1,6 @@
+python function 80% (8/10)
+python  operator 90% (5/5)
+python data type 100% (9/10)
+python  output 90% (9/10)
+python  Miscellaneous 100% (7/10)
+python Assorted MCQs on Python  not completly done.
